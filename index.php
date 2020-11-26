0dd362809f95e01baa5446eb7b54dd927198d755
Mon Nom d'utilisateur <nmarrocchi@la-providence.net>
2020-11-26 16:49:21 +0100

    Index

--- index.php
+++ index.php
@@ -2,10 +2,27 @@
 <html lang="fr">
     <head>
         <meta charset="UTF-8">
+        <link rel="icon" type="image/png" href="img/icon.png" />
+        <link rel="stylesheet" type="text/css" href="style.css" />
         <title>Azur Lane</title>
     </head>
 
     <body>
+
+        <div class="Header">
+            <img src="img/Logo_2.png"alt=""> 
+
+            <div class="Menu">
+                    <ul>
+                        <li>Personnages</li>
+                        <li>Factions</li>
+                        <li>histoire</li>
+                        <li>adaptation en animé</li>
+                    </ul>
+            </div>
+        </div>
+
+    
 
     </body>
 </html>
