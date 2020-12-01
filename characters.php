cc9478017a1f4faed2bfee053b3a070d5a5f7467
Mon Nom d'utilisateur <nmarrocchi@la-providence.net>
2020-12-01 17:14:41 +0100

    PHP

--- characters.php
+++ characters.php
@@ -10,15 +10,11 @@
 
     <?php
 
-    $_POST["Order"] = 'Rarity';
-    $_POST["Order_Type"] = 'DESC';
-
-    $ID=0;
         try{
 
 $BDD = new PDO('mysql:host=192.168.65.60; dbname=azurlane; charset=utf8','AzurLane', 'azurlane');
 
-$Result = $BDD->query('SELECT * FROM characters ORDER BY '.$_POST["Order"].' '.$_POST["Order_Type"].'');
+$Result = $BDD->query('SELECT * FROM characters ORDER BY Rarity DESC ');
 
 }catch(Exception $e){
 
@@ -32,9 +28,11 @@ $Result = $BDD->query('SELECT * FROM characters ORDER BY '.$_POST["Order"].' '.$
 
         <?php include("Header.php"); ?>
 
-        <div class="contenu">
+        <div class="Contenu">
 
 <?php
+
+        $ID = 0;
 
         while ( $Data = $Result->fetch() )   
  {  $ID++;
@@ -54,106 +52,98 @@ $Result = $BDD->query('SELECT * FROM characters ORDER BY '.$_POST["Order"].' '.$
      }
 
      switch($Data["Affiliation"]){
-        case Dragon_Empery: $Data["Affiliation"] = ' ';
+        case 1: $Data['Affiliation'] = 'Dragon Empery';
+     break;
+        case 2: $Data["Affiliation"] = 'Eagle Union';
     break;
-        case Eagle_Union: $Data["Affiliation"] = ' ';
+        case 3: $Data["Affiliation"] = 'Iris Libre';
     break;
-        case Iris_Libre: $Data["Affiliation"] = ' ';
+        case 4: $Data["Affiliation"] = 'Iron Blood';
     break;
-        case Iron_Blood: $Data["Affiliation"] = ' ';
+        case 5: $Data["Affiliation"] = 'Northem Parliament ';
     break;
-        case Northem_Parliament: $Data["Affiliation"] = ' ';
+        case 6: $Data["Affiliation"] = 'Royal Navy';
     break;
-    case Royal_Navy: $Data["Affiliation"] = ' ';
-break;
+        case 7: $Data["Affiliation"] = 'Sakura Empire';
+    break;
+        case 8: $Data["Affiliation"] = 'Sardegna Empire';
+    break;
+        case 9: $Data["Affiliation"] = 'Universal';
+    break;
+        case 10: $Data["Affiliation"] = 'Vichya Dominion';
+    break;
+        case 11: $Data["Affiliation"] = 'BiliBili';
+    break;
+        case 12: $Data["Affiliation"] = 'HoloLive';
+    break;
+        case 13: $Data["Affiliation"] = 'Kizuna AI';
+    break;
+        case 14: $Data["Affiliation"] = 'Neptunia';
+    break;
+        case 15: $Data["Affiliation"] = 'Utawarerumono';
+    break;
+        case 16: $Data["Affiliation"] = 'Venus Vacation';
+    break;
 
     default: $Data["Affiliation"] = 'None';
     }
 ?>
 
-            <div class="Characters_Infos">
+                <div class="Characters_Infos">
 
-                <div class="Characters_Icon">
+                    <div class="Characters_Icon">
+                        <p><?php echo $Data["ShipName"]; ?></p>
+                        <img class="Characters_Type" src="img/Type/<?php echo $Data["TypeOfShip"]; ?>.png" alt="Type">
+                        <img class="Characters_Img" src="img/Characters/<?php echo $Data["ShipName"]; ?>.png" alt="Character">
+                        
+                    </div>
+                    <div class="Characters_Stats">
 
-                    <div class="Character_Name"><?php echo $Data["ShipName"]; ?></div>
+                        <table class="Basic_Values">
+                            <tr>
+                                <thead class="THead">
+                                    <td class="h_Rarity">Rarity</td>
+                                    <td class="h_TypeOfShip">Type Of Ship</td>
+                                    <td class="h_Affiliation" colspan="3">Nationality</td>
+                                </thead>
+                            </tr>
+                            <tr class="Values">
+                                <td class="Rarity"><?php echo $Data["Rarity"]; ?></td>
+                                <td class="TypeOfShip"><?php echo $Data["TypeOfShip"]; ?></td>
+                                <td class="Affiliation" colspan="3"><img src="img/Affiliation/<?php echo $Data["Affiliation"]; ?>.png" alt="Affiliation"><p><?php echo $Data["Affiliation"]; ?></p></td>
+                            </tr>
+                        </table>
 
-                    <div class="Character">
-                        <img class="Type" src="img/Type/<?php echo $Data["TypeOfShip"]; ?>.png">
+                        <table class="Stats_Values">
+                            <tr>
+                                <thead class="THead">
+                                    <td class="h_Firepower">Firepower</td>
+                                    <td class="h_Health">Health</td>
+                                    <td class="h_AntiAir">Anti-Air</td>
+                                    <td class="h_Evasion">Evasion</td>
+                                    <td class="h_Aviation">Aviation</td>
+                                    <td class="h_Torpedo">Torpedo</td>
+                                </thead>
+                            </tr>
+                                <tr class="Values">
+                                <td class="Firepower"><?php echo $Data["Firepower"]; ?></td>
+                                <td class="Health"><?php echo $Data["Health"]; ?></td>
+                                <td class="AntiAir"><?php echo $Data["AntiAir"]; ?></td>
+                                <td class="Evasion"><?php echo $Data["Evasion"]; ?></td>
+                                <td class="Aviation"><?php echo $Data["Aviation"]; ?></td>
+                                <td class="Torpedo"><?php echo $Data["Torpedo"]; ?></td>
+                            </tr>
+                        </table>
                     </div>
 
                 </div>
 
-                <div class="Stats">
-                    <table>
-                        <tr>
-                            <thead class="THead">
-                                <td class="h_ID">ID</td>
-                                <td class="h_ShipName">Ship Name</td>
-                                <td class="h_Rarity">Rarity</td>
-                                <td class="h_TypeOfShip">Type Of Ship</td>
-                                <td class="h_Affiliation">Affiliation</td>
-                            </thead>
-                        </tr>
-                        <tr>
-                            <td class="ID"><?php echo($ID) ?></td>
-                            <td class="ShipName"><?php echo $Data["ShipName"]; ?><p><img src="img/Characters/<?php echo $Data["ShipName"]; ?>.png"></p></td>
-                            <td class="Rarity"><?php echo $Data["Rarity"]; ?></td>
-                            <td class="TypeOfShip"><?php echo $Data["TypeOfShip"]; ?></td>
-                            <td class="Affiliation"><?php echo $Data["Affiliation"]; ?></td>
-                        </tr>
-                        <tr>
-                            <thead class="THead">
-                                <td class="h_Firepower">Firepower</td>
-                                <td class="h_Health">Health</td>
-                                <td class="h_AntiAir">Anti-Air</td>
-                                <td class="h_Evasion">Evasion</td>
-                                <td class="h_Aviation">Aviation</td>
-                                <td class="h_Torpedo">Torpedo</td>
-                            </thead>
-                        </tr>
-                        <tr>
-                            <td class="Firepower"><?php echo $Data["Firepower"]; ?></td>
-                            <td class="Health"><?php echo $Data["Health"]; ?></td>
-                            <td class="AntiAir"><?php echo $Data["AntiAir"]; ?></td>
-                            <td class="Evasion"><?php echo $Data["Evasion"]; ?></td>
-                            <td class="Aviation"><?php echo $Data["Aviation"]; ?></td>
-                            <td class="Torpedo"><?php echo $Data["Torpedo"]; ?></td>
-                        </tr>
-                    </table>
-                </div>
-
-            </div>
-                    
             <?php
 
 }
 $Result->closeCursor();
 
 ?>
-
-
-
-                    <tr>
-                        <td class="ID"><?php echo($ID) ?></td>
-                        <td class="ShipName"><?php echo $Data["ShipName"]; ?><p><img src="img/Characters/<?php echo $Data["ShipName"]; ?>.png"></p></td>
-                        <td class="Rarity"><?php echo $Data["Rarity"]; ?></td>
-                        <td class="TypeOfShip"><?php echo $Data["TypeOfShip"]; ?></td>
-                        <td class="Affiliation"><?php echo $Data["Affiliation"]; ?></td>
-                        <td class="Firepower"><?php echo $Data["Firepower"]; ?></td>
-                        <td class="Health"><?php echo $Data["Health"]; ?></td>
-                        <td class="AntiAir"><?php echo $Data["AntiAir"]; ?></td>
-                        <td class="Evasion"><?php echo $Data["Evasion"]; ?></td>
-                        <td class="Aviation"><?php echo $Data["Aviation"]; ?></td>
-                        <td class="Torpedo"><?php echo $Data["Torpedo"]; ?></td>
-                    </tr>
-
-
-
-
-
-                </table>
-
-            </div>
 
         </div>
 
